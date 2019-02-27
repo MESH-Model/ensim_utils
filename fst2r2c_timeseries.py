eb3c1800995b2088c26f0f72c17e7aaebf635ac0
dprincz <17090685+dprincz@users.noreply.github.com>
2019-02-27 16:28:55 -0600

    Updates for science
    Updated file paths for science (`site2`). Commented the dependency on `timezonefinder`. Added `utctimetofstfname_gdps` for GDPS. Relabeled 'gem' to 'RDPS' but preserved `utctimetofstfname_gem`, pointing to `utctimetofstfname_rdps`. Updated `utctimetofstfname_capa` to return `ip2` value. Updated the default dates in `r2ctimeseriesfromfst`.

--- fst2r2c_timeseries.py
+++ fst2r2c_timeseries.py
@@ -12,17 +12,15 @@ from ensim_utils import *
 # pip2 install --user 'timezonefinder<3.2.1'
 # Version 3.2.1 seems to have an error.
 
-from timezonefinder import TimezoneFinder
+###from timezonefinder import TimezoneFinder
 
 # To load rpnpy:
 # . s.ssmuse.dot ENV/py/2.7/rpnpy/2.0.4
 
 # Archive paths.
 
-PATH_ARMNMSH = '/fs/cetus/fs3/mrb/armn/armnmsh/mrb2/armnmsh'
-PATH_RARC_MISSING = '/fs/cetus3/fs2/cmd/e/afsg/dap/cetus3/sa_mesh_forcing/rarc'
-PATH_ARMNMSH = '/fs/cetus3/fs2/cmd/e/afsg/dap/RDRS_CaPA24h_prelim/fst'
-PATH_RARC_MISSING = ''
+PATH_ARMNMSH = '/fs/site2/dev/eccc/mrd/rpnenv/smsh001'
+PATH_RARC_MISSING = '/fs/site2/dev/eccc/cmd/n/dap000/sa_mesh_forcing/rarc'
 
 def utctimetofstfname_rdrs(utctime):
 
@@ -36,7 +34,7 @@ def utctimetofstfname_rdrs(utctime):
 
         return { 'path': fstsrcpath, 'ip2': filetime.hour }
 
-def utctimetofstfname_gem(utctime):
+def utctimetofstfname_rdps(utctime):
 
         # 00:00->05:00 ; 12-hour forecast of yesterday.
 
@@ -78,24 +76,69 @@ def utctimetofstfname_gem(utctime):
 
         return { 'path': fstsrcpath, 'ip2': filetime.hour }
 
+def utctimetofstfname_gem(utctime):
+
+        # RDPS.
+
+        return utctimetofstfname_rdps(utctime)
+
+def utctimetofstfname_gdps(utctime):
+
+        # 00:00->05:00 ; 12-hour forecast of yesterday.
+
+        if (utctime.hour < 6):
+                filetime = utctime + dt.relativedelta(hours = -12)
+                filefcst = 12
+
+	# 06:00->17:00 ; 00-hour forecast of today.
+
+	elif (utctime.hour < 18):
+		filetime = utctime
+		filefcst = 0
+
+        # 18:00->23:00 ; 12-hour forecast of today.
+
+        else:
+                filetime = utctime + dt.relativedelta(hours = -12)
+                filefcst = 12
+
+        # Special rules.
+
+        fstsrcpath = PATH_ARMNMSH + ('/arcsfc/%04d/%02d/%02d/glbeta/%04d%02d%02d%02d_%03d' % (filetime.year, filetime.month, filetime.day, filetime.year, filetime.month, filetime.day, filefcst, filetime.hour))
+
+        # Check rarc backup if file does not exist.
+
+        if (not path.exists(fstsrcpath)):
+                fstsrcpath = PATH_RARC_MISSING + ('/operation.forecasts.glbeta/%04d%02d%02d%02d_%03d' % (filetime.year, filetime.month, filetime.day, filefcst, filetime.hour))
+
+        # Stop if the path does not exist.
+
+        if (not path.exists(fstsrcpath)):
+                print('ERROR: Path does not exist. Script cannot continue. ' + fstsrcpath)
+                exit()
+
+        # Return file path and adjust ip2.
+
+        return { 'path': fstsrcpath, 'ip2': filetime.hour }
+
 def utctimetofstfname_capa(utctime):
 
         # 00:00 -> 06:00 ; 06-h.
 
         if (utctime.hour < 6):
-                filetime = utctime + dt.relativedelta(hour = 0)
+                filetime = utctime + dt.relativedelta(hour = 6)
                 filefcst = 6
 
         # 06:00 -> 12:00 ; 12-h.
 
         elif (utctime.hour < 12):
-                filetime = utctime + dt.relativedelta(hour = 0)
+                filetime = utctime + dt.relativedelta(hour = 12)
                 filefcst = 12
 
         # 12:00 -> 18:00 ; 18-h.
 
         elif (utctime.hour < 18):
-                filetime = utctime + dt.relativedelta(hour = 0)
+                filetime = utctime + dt.relativedelta(hour = 18)
                 filefcst = 18
 
         # 18:00 -> 00:00 + 1 day ; 00-h + 1-day.
@@ -124,13 +167,13 @@ def utctimetofstfname_capa(utctime):
 
         # Return file path and adjust ip2.
 
-        return { 'path' : fstsrcpath, 'ip2' : -1 }
+        return { 'path' : fstsrcpath, 'ip2' : filetime.hour }
 
 def r2ctimeseriesfromfst(
 	R2CSHED_INFILE = 'MESH_drainage_database.r2c',
 	PROCESS_FSTCONVFLD = [],
-	START_TIME = datetime(year = 2010, month = 1, day = 1, hour = 12, tzinfo = tz.tzutc()),
-	STOP_BEFORE_TIME = datetime(year = 2015, month = 1, day = 1, hour = 12, tzinfo = tz.tzutc()),
+	START_TIME = datetime(2004, 10, 1, tzinfo = tz.tzutc()),
+	STOP_BEFORE_TIME = datetime(2012, 10, 1, tzinfo = tz.tzutc()),
 	I_COUNTER = 1,
 	LOCAL_TIME_ZONE = tz.tzutc()
 	):
@@ -145,17 +188,17 @@ def r2ctimeseriesfromfst(
 	# Default parameters.
 
 	if (not PROCESS_FSTCONVFLD):
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_temperature.r2c', framefreq = 60, fstnomvar = 'TT', AttributeName = 'Air_temperature_at_40m', AttributeUnits = 'K', intpopt = rmn.EZ_INTERP_LINEAR, constadd = 273.16))
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_humidity.r2c', framefreq = 60, fstnomvar = 'HU', AttributeName = 'Specific_humidity_40m', AttributeUnits = 'kg kg**-1', intpopt = rmn.EZ_INTERP_LINEAR))
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_pres.r2c', framefreq = 60, fstnomvar = 'P0', AttributeName = 'Air_pressure_at_surface', AttributeUnits = 'Pa', intpopt = rmn.EZ_INTERP_LINEAR, constmul = 100.0))
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_longwave.r2c', framefreq = 60, fstnomvar = 'FI', AttributeName = 'Incoming_longwave_down_incident_at_surface', AttributeUnits = 'W m**-2', intpopt = rmn.EZ_INTERP_LINEAR))
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_shortwave.r2c', framefreq = 60, fstnomvar = 'FB', AttributeName = 'Incoming_shortwave_down_incident_at_surface', AttributeUnits = 'W m**-2', intpopt = rmn.EZ_INTERP_LINEAR))
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_wind.r2c', framefreq = 60, fstnomvar = 'UV', AttributeName = 'Wind_speed_at_40m', AttributeUnits = 'm s**-1', intpopt = rmn.EZ_INTERP_LINEAR, constmul = 0.5144444444444444444))
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_winddir.r2c', framefreq = 60, fstnomvar = 'WD', AttributeName = 'Wind_direction_at_40m', AttributeUnits = 'degrees', intpopt = rmn.EZ_INTERP_LINEAR))
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_wind_u-component.r2c', framefreq = 60, fstnomvar = 'UU', AttributeName = 'Wind_speed_U-component_at_40m', AttributeUnits = 'm s**-1', intpopt = rmn.EZ_INTERP_LINEAR, constmul = 0.5144444444444444444))
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_wind_v-component.r2c', framefreq = 60, fstnomvar = 'VV', AttributeName = 'Wind_spped_V-component_at_40m', AttributeUnits = 'm s**-1', intpopt = rmn.EZ_INTERP_LINEAR, constmul = 0.5144444444444444444))
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_rainacc.r2c', framefreq = 60, fstnomvar = 'PR0', AttributeName = 'Total_precipitation_accumulated_at_surface', AttributeUnits = 'kg m**-2', constmul = 1000.0))
-		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_rain.r2c', framefreq = 60, fstnomvar = 'PR0', AttributeName = 'Total_precipitation_rate_at_surface', AttributeUnits = 'kg m**-2 s**-1', constmul = 0.2777777777777778))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_temperature.r2c', fstnomvar = 'TT', AttributeName = 'Air_temperature_at_40m', AttributeUnits = 'K', fstip1 = 11950, intpopt = rmn.EZ_INTERP_LINEAR, constadd = 273.16))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_humidity.r2c', fstnomvar = 'HU', AttributeName = 'Specific_humidity_40m', AttributeUnits = 'kg kg**-1', fstip1 = 11950, intpopt = rmn.EZ_INTERP_LINEAR))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_pres.r2c', fstnomvar = 'P0', AttributeName = 'Air_pressure_at_surface', AttributeUnits = 'Pa', intpopt = rmn.EZ_INTERP_LINEAR, constmul = 100.0))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_longwave.r2c', fstnomvar = 'FI', AttributeName = 'Incoming_longwave_down_incident_at_surface', AttributeUnits = 'W m**-2', intpopt = rmn.EZ_INTERP_LINEAR))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_shortwave.r2c', fstnomvar = 'FB', AttributeName = 'Incoming_shortwave_down_incident_at_surface', AttributeUnits = 'W m**-2', intpopt = rmn.EZ_INTERP_LINEAR))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_wind.r2c', fstnomvar = 'UV', AttributeName = 'Wind_speed_at_40m', AttributeUnits = 'm s**-1', fstip1 = 11950, intpopt = rmn.EZ_INTERP_LINEAR, constmul = 0.5144444444444444444))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_winddir.r2c', fstnomvar = 'WD', AttributeName = 'Wind_direction_at_40m', AttributeUnits = 'degrees', fstip1 = 11950, intpopt = rmn.EZ_INTERP_LINEAR))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_wind_u-component.r2c', fstnomvar = 'UU', AttributeName = 'Wind_speed_U-component_at_40m', AttributeUnits = 'm s**-1', fstip1 = 11950, intpopt = rmn.EZ_INTERP_LINEAR, constmul = 0.5144444444444444444))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_wind_v-component.r2c', fstnomvar = 'VV', AttributeName = 'Wind_spped_V-component_at_40m', AttributeUnits = 'm s**-1', fstip1 = 11950, intpopt = rmn.EZ_INTERP_LINEAR, constmul = 0.5144444444444444444))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_rainacc.r2c', fstnomvar = 'PR0', AttributeName = 'Total_precipitation_accumulated_at_surface', AttributeUnits = 'kg m**-2', constmul = 1000.0))
+		PROCESS_FSTCONVFLD.append(r2cconversionfieldfromfst(fpathr2cout = 'basin_rain.r2c', fstnomvar = 'PR0', AttributeName = 'Total_precipitation_rate_at_surface', AttributeUnits = 'kg m**-2 s**-1', constmul = 0.2777777777777778))
 
 	# Read the header from the r2c input file.
 
@@ -169,14 +212,14 @@ def r2ctimeseriesfromfst(
 	# Derive timezone from centre location in grid.
 	# Add DST offset to print only standard time to file (to avoid irregular time-stamps).
 
-	if (START_TIME.tzinfo is None):
-		if (r2c.grid.Projection.lower() == 'latlong'):
-			clon = r2c.grid.xOrigin + (r2c.grid.xCount + 1)*r2c.grid.xDelta/2.0
-			clat = r2c.grid.yOrigin + (r2c.grid.yCount + 1)*r2c.grid.yDelta/2.0
-			ctz = TimezoneFinder().timezone_at(lng = clon, lat = clat)
-			LOCAL_TIME_ZONE = tz.gettz(ctz)
-		START_TIME = START_TIME.replace(tzinfo = LOCAL_TIME_ZONE)
-		STOP_BEFORE_TIME = STOP_BEFORE_TIME.replace(tzinfo = LOCAL_TIME_ZONE)
+###	if (START_TIME.tzinfo is None):
+###		if (r2c.grid.Projection.lower() == 'latlong'):
+###			clon = r2c.grid.xOrigin + (r2c.grid.xCount + 1)*r2c.grid.xDelta/2.0
+###			clat = r2c.grid.yOrigin + (r2c.grid.yCount + 1)*r2c.grid.yDelta/2.0
+###			ctz = TimezoneFinder().timezone_at(lng = clon, lat = clat)
+###			LOCAL_TIME_ZONE = tz.gettz(ctz)
+###		START_TIME = START_TIME.replace(tzinfo = LOCAL_TIME_ZONE)
+###		STOP_BEFORE_TIME = STOP_BEFORE_TIME.replace(tzinfo = LOCAL_TIME_ZONE)
 
 	# Read header from r2c input file and create r2c output files.
 
@@ -199,7 +242,7 @@ def r2ctimeseriesfromfst(
 
                 # Open file.
 
-                fstsrc = utctimetofstfname_rdrs(FST_CURRENT_TIME)
+                fstsrc = utctimetofstfname_gem(FST_CURRENT_TIME)
                 fstfid = rmn.fstopenall(fstsrc['path'])
 
                 # Records.
